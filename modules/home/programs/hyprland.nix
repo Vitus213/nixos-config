489e43747b8ef266f13f22c897557f5d6160b054
Vitus <zhzvitus@gmail.com>
2025-10-15 10:36:56 +0800

    again failure

--- modules/home/programs/hyprland.nix
+++ modules/home/programs/hyprland.nix
@@ -3,7 +3,9 @@
     # Whether to enable Hyprland wayland compositor
     enable = true;
     # The hyprland package to use
-    package = pkgs.hyprland;
+  # Use the same hyprland package coming from the flake inputs to avoid
+  # version/protocol mismatches between system and user packages.
+  package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     # Whether to enable XWayland
     xwayland.enable = true;
 
