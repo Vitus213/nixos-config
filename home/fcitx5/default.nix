244f85aafc19a4b413916885bd759f52f70f45f0
Vitus <zhzvitus@gmail.com>
2025-10-18 22:25:38 +0800

    把rime配置添加了进来

--- home/fcitx5/default.nix
+++ home/fcitx5/default.nix
@@ -12,7 +12,7 @@
           rimeDataPkgs = [
             # 小鹤音形配置，配置来自 flypy.com 官方网盘的鼠须管配置压缩包「小鹤音形“鼠须管”for macOS.zip」
             # 我仅修改了 default.yaml 文件，将其中的半角括号改为了直角括号「 与 」。
-            ./rime-data-flypy
+            ./rime-data-vitus
           ];
         };
       };
