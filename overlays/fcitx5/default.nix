6b6e33da67f7dbbae1e643063d1e4e2b79237475
Vitus <zhzvitus@gmail.com>
2025-11-17 16:10:10 +0800

    对 add overlay &&fcitx5的overlay没有用

---
+++ overlays/fcitx5/default.nix
@@ -0,0 +1,7 @@
+final: prev:{
+  rime-data = ./rime-data-vitus;
+  fcitx5-rime = prev.fcitx5-rime.override { rimeDataPkgs = [ ./rime-data-vitus ]; };
+
+  # used by macOS Squirrel
+  flypy-squirrel = ./rime-data-vitus;
+}
