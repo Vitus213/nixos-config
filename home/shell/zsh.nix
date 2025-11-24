5e786284eed5a91abbece781c8720ad98d204fc8
Vitus <zhzvitus@gmail.com>
2025-11-25 01:56:18 +0800

    fix: - 修改windowsrule fix: - add zsh configure

--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -26,16 +26,33 @@
       ignoreDups = true;
     };
 
+    shellAliases = {
+      np = "unset http_proxy https_proxy all_proxy";
+      p =
+        "export http_proxy=http://127.0.0.1:7897 https_proxy=http://127.0.0.1:7897 all_proxy=socks5://127.0.0.1:7897";
+      ls = "ls --color=auto";
+      la = "ls -a";
+      ll = "ls -l";
+    };
+    sessionVariables = {
+      http_proxy = "http://127.0.0.1:7897";
+      https_proxy = "https://127.0.0.1:7897";
+      all_proxy = "socks5://127.0.0.1:7897";
+      no_proxy = "localhost,127.0.0.1,::1";
+
+      # ⚠️ 安全警告：不要将 API Key 直接写在 git 管理的代码里
+      # 建议使用 agenix, sops-nix 或从外部文件读取
+      ANTHROPIC_AUTH_TOKEN =
+        "sk-jYRZIe8JFq4Y3VesvYnAnwbtLj4Q2R7q0J0Tyw6ivxE0KVBN";
+      ANTHROPIC_BASE_URL = "https://anyrouter.top";
+      # 输入法设置 (如果是 Home Manager 管理，可以在这里设；
+      # 如果是 NixOS 系统级配置，通常在 i18n.inputMethod 中设置会自动生效)
+      GTK_IM_MODULE = "fcitx";
+      QT_IM_MODULE = "fcitx";
+      XMODIFIERS = "@im=fcitx";
+    };
     initContent = ''
-      alias ls="ls --color=auto"
-      alias la="ls -a"
-      alias ll="ls -l"
-       [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-      export ANTHROPIC_AUTH_TOKEN=sk-jYRZIe8JFq4Y3VesvYnAnwbtLj4Q2R7q0J0Tyw6ivxE0KVBN 
-      export ANTHROPIC_BASE_URL=https://anyrouter.top
-      export GTK_IM_MODULE=fcitx
-      export QT_IM_MODULE=fcitx
-      export XMODIFIERS="@im=fcitx"
+      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
     '';
   };
 
