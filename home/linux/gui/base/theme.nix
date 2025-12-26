2c73ad02c651e7a3fdc87bd30d2a7bec40a32ef4
Vitus213 <zhzvitus@gmail.com>
2025-12-26 23:22:34 +0800

    fix

--- home/linux/gui/base/theme.nix
+++ home/linux/gui/base/theme.nix
@@ -15,10 +15,6 @@
     flavor = "mocha";
     # one of "blue", "flamingo", "green", "lavender", "maroon", "mauve", "peach", "pink", "red", "rosewater", "sapphire", "sky", "teal", "yellow"
     accent = "pink";
-    # 使用 catppuccin cursor
-    cursors.enable = lib.mkForce true;
-    delta.enable = true;
-    bat.enable = true;
-    kitty.enable = true;
+
   };
 }
