dc26bc7760836668c4fce34641566773f1be9f69
Vitus213 <zhzvitus@gmail.com>
2025-12-30 20:53:53 +0800

    - fix:调整路径为软链接

--- home/linux/gui/base/desktop/default.nix
+++ home/linux/gui/base/desktop/default.nix
@@ -28,6 +28,10 @@
   };
 
   home.packages = with pkgs; [
+    hypridle
+    wlogout
+    waybar
+    mako
     swaybg # the wallpaper
     wl-clipboard # copying and pasting
     hyprpicker # color picker
@@ -42,7 +46,7 @@
   xdg.configFile =
     let
       mkSymlink = config.lib.file.mkOutOfStoreSymlink;
-      confPath = ./conf;
+      confPath = "${config.home.homeDirectory}/nixos-config/home/linux/gui/base/desktop/conf";
     in
     {
       "mako".source = mkSymlink "${confPath}/mako";
@@ -50,25 +54,19 @@
       "wlogout".source = mkSymlink "${confPath}/wlogout";
       "hypr/hypridle.conf".source = mkSymlink "${confPath}/hypridle.conf";
     };
-  # status bar
-  programs.waybar = {
-    enable = true;
-    systemd.enable = true;
-  };
   # Disable catppuccin to avoid conflict with my non-nix config.
   # catppuccin.waybar.enable = false;
 
-  # screen locker
-  programs.swaylock.enable = true;
+
 
   # Logout Menu
-  programs.wlogout.enable = true;
+
   # catppuccin.wlogout.enable = false;
 
   # Hyprland idle daemon
-  services.hypridle.enable = true;
+  # services.hypridle.enable = true;
 
   # notification daemon, the same as dunst
-  services.mako.enable = true;
+  # services.mako.enable = true;
   #   catppuccin.mako.enable = false;
 }
